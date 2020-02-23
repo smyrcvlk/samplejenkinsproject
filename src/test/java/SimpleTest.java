ed6ad1a95d69099e7dfded9c67a8bbbf3dde7321
smyrcvlk <smyrcvlk@gmail.com>
2020-02-23 12:21:00 -0500

    jenkins

---
+++ src/test/java/SimpleTest.java
@@ -0,0 +1,12 @@
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.Test;
+
+public class SimpleTest {
+@Test
+    public void passingTest(){
+    System.out.println("this test will always pass");
+}
+
+
+
+}
